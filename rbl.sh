d50cf7819943c8c9db181bfde658f93b0f1a23e0
splitice <mat999@gmail.com>
2014-08-15 14:25:55 +1000

    added support for listing by CIDR and added concurrency to details action

--- rbl.sh
+++ rbl.sh
@@ -4,12 +4,20 @@
 ## Developed By: SplitIce <https://www.x4b.net>
 ## Free for commercial & Non-commercial use or whatever, just dot sue me.
 ##
-## Usage: 
+## Usage:
 ## ./rbl.sh listcheck - Verify that all DNSBLS in the list are responding within a reasonable time (online)
 ## ./rbl.sh details [ip1] [...] - Fetch details for all RBL entries for one or many IP addresses
 ## ./rbl.sh count [ip1] [...] - Fetch a total count of RBL entries for many IP addresses
 
 RBL=rbl_list.txt
+
+function process_ip {
+        if [[ $1 == */* ]]; then
+                echo "$(prips $1)"
+        else
+                echo "$1"
+        fi
+}
 
 if [[ $1 == "check" ]]; then
         dig +short +time=1 +tries=2 $2 | grep "127.0.0."
@@ -21,42 +29,49 @@ elif [[ $1 == "listcheck" ]]; then
                         echo "$var timed out"
                 fi
         done <<< "$RBL_C"
-elif [[ $1 == "details" ]]; then
+elif [[ $1 == "detailscheck" ]]; then
         echo "Performing detailed lookup for $2"
+
         RBL_C=$(cat "$RBL" | grep -v "#")
+		W=$( echo $2 | cut -d. -f1 )
+		X=$( echo $2 | cut -d. -f2 )
+		Y=$( echo $2 | cut -d. -f3 )
+		Z=$( echo $2 | cut -d. -f4 )
 
-		for i in "${@:2}"; do
-			W=$( echo $i | cut -d. -f1 )
-			X=$( echo $i | cut -d. -f2 )
-			Y=$( echo $i | cut -d. -f3 )
-			Z=$( echo $i | cut -d. -f4 )
+		while read -r var; do
+			CHECK=$(dig +short $Z.$Y.$X.$W.$var | grep "127.0.0." | wc -l)
 
-			while read -r var; do
-					CHECK=$(dig +short $Z.$Y.$X.$W.$var | grep "127.0.0." | wc -l)
-
-					if [[ $CHECK -gt 0 ]]; then
+			if [[ $CHECK -gt 0 ]]; then
 							REASON=$(dig +short $Z.$Y.$X.$W.$var TXT)
 							echo "$i blacklisted on $var for: $REASON"
-					fi
-			done <<< "$RBL_C"
-		done
+			fi
+		done <<< "$RBL_C"
+elif [[ $1 == "details" ]]; then
+        for var_i in "${@:2}"
+        do
+            echo "$(process_ip $var_i)" | parallel --max-procs 100 bash "$0" detailscheck {}
+        done
 elif [[ $1 == "count" ]]; then
         COUNT=0
         RBL_C=$(cat "$RBL" | grep -v "#")
 
-        for var in "${@:2}"
+        for var_i in "${@:2}"
         do
+            while read -r var; do
                 W=$( echo $var | cut -d. -f1 )
                 X=$( echo $var | cut -d. -f2 )
                 Y=$( echo $var | cut -d. -f3 )
                 Z=$( echo $var | cut -d. -f4 )
                 R=$(echo "$RBL_C" | sed -e "s/^/$Z.$Y.$X.$W./g" | parallel --max-procs 100 bash "$0" check {} | wc -l)
                 let "COUNT=R+COUNT"
+            done <<< "$(process_ip $var_i)"
         done
         echo $COUNT
 else
-	echo "Usage:" 
-	echo "$0 listcheck - Verify that all DNSBLS in the list are responding within a reasonable time (online)"
-	echo "$0 details [ip1] [...] - Fetch details for all RBL entries for one or many IP addresses"
-	echo "$0 count [ip1] [...] - Fetch a total count of RBL entries for many IP addresses"
+        echo "Usage:"
+        echo "$0 listcheck - Verify that all DNSBLS in the list are responding within a reasonable time (online)"
+        echo "$0 details [ip1] [...] - Fetch details for all RBL entries for one or many IP addresses"
+        echo "$0 count [ip1] [...] - Fetch a total count of RBL entries for many IP addresses"
+		echo ""
+		echo "IP networks in CIDR form may also be specified"
 fi
